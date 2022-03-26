1ccf9ea586eda0177785844e7c098e2dcc3f7cdc
ishank <ishankbansal838@gmail.com>
2022-03-26 15:44:58 +0530

    added generateKey for url

--- api/pkg/keyGenerator/keyGenerator.go
+++ api/pkg/keyGenerator/keyGenerator.go
@@ -1,1 +1,18 @@
 package keygenerator
+
+import (
+	"crypto/md5"
+	"encoding/base64"
+)
+
+func GenerateKey(url string) string {
+	hash := makeHash(url)
+
+	key := base64.StdEncoding.EncodeToString(hash)
+	return key
+}
+
+func makeHash(url string) []byte {
+	hash := md5.Sum([]byte(url))
+	return hash[:]
+}
